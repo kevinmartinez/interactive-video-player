db7bfb12c71cacb2bb2130853089be22f80ab70b
kevinmartinez <hi.kevin.martinez@gmail.com>
2019-11-05 18:13:35 +0100

    add time event listener and toggle class WIP

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
 console.log('program start')
+
+// Select caption spans
+const captions = document.querySelectorAll('span');
+
+// Log current video time
+const video = document.querySelector('.video');
+
+video.addEventListener('timeupdate', (event) => {
+
+  // Loop through captions
+  // Loop through captions
+  captions.forEach(item => {
+    // console.log(item.dataset)
+
+    // Add highlight class dependent on currenTime
+    if(video.currentTime > item.dataset.start && video.currentTime < item.dataset.end) {
+      console.dir(item)
+      console.log(item.textContent)
+
+      // TODO: Research IF toggle can be used
+      // item.classList.toggle('highlight');
+      item.classList.add('highlight')
+    } else {
+      item.classList.remove('highlight')
+    }
+
+  })
+
+  // console.log(event)
+
+  // // Check timestamp. TODO: What is timeStamp? Who is timeStamp?
+  // console.log(event.timeStamp)
+
+  // // Check current time
+  // console.log(video.currentTime)
+  // console.log('The currentTime attribute has been updated. Again.');
+});
+
+// TODO: Make caption <spans> clickable so that the video plays form captions timestamp (data-start => data-end)
