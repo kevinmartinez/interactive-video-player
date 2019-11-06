eb631322d565acf26cea28a92072b20b36348fcb
kevinmartinez <hi.kevin.martinez@gmail.com>
2019-11-06 21:06:20 +0100

    remove console logs

--- js/app.js
+++ js/app.js
@@ -5,10 +5,12 @@ const container = document.querySelector('.captions')
 
 // Select caption spans
 const captions = document.querySelectorAll('span');
+console.dir(captions)
 
-// Log current video time
+// Select video
 const video = document.querySelector('.video');
 
+// TODO: Make caption <spans> clickable so that the video plays form captions timestamp (data-start => data-end)
 // Let user click on captions
 container.addEventListener('click', e => {
 
@@ -22,38 +24,17 @@ container.addEventListener('click', e => {
   }
 })
 
-
 video.addEventListener('timeupdate', (event) => {
 
   // Loop through captions
-  // Loop through captions
   captions.forEach(item => {
-    // console.log(item.dataset)
-
-    // Add highlight class dependent on currenTime
+  
+    // Add highlight class dependent on currentTime
     if(video.currentTime > item.dataset.start && video.currentTime < item.dataset.end) {
-      console.dir(item)
-      // console.log(item.textContent)
-
-      // TODO: Research IF toggle can be used
-      // item.classList.toggle('highlight');
       item.classList.add('highlight')
-      //item.classList.toggle('highlight');
     } else {
       item.classList.remove('highlight')
-      // item.classList.toggle('highlight', false);
     }
 
   })
-
-  // console.log(event)
-
-  // // Check timestamp. TODO: What is timeStamp? Who is timeStamp?
-  // console.log(event.timeStamp)
-
-  // // Check current time
-  // console.log(video.currentTime)
-  // console.log('The currentTime attribute has been updated. Again.');
 });
-
-// TODO: Make caption <spans> clickable so that the video plays form captions timestamp (data-start => data-end)
